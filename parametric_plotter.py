273a3761561ad164fe80efb97ddaa667d944ae88
Divyajeet Singh <knightt1821@gmail.com>
2022-08-19 00:34:52 +0530

    Add files via upload

--- parametric_plotter.py
+++ parametric_plotter.py
@@ -4,20 +4,22 @@ import numpy as np
 ################################################################################
 
 # CONSTANTS
-r = 10
-a, b = 10, 5
-k1, k2 = 3, 2
-def sec(x):
-    return 1 / np.cos(x)
+R: float = 10.0
+A: float = 10.0
+B: float = 5
+K1: float = 3
+K2: float = 2
 
 ################################################################################
 
-# generate a large list of inputs (floats)
-t = theta = np.arange(-450, 450.015625, 0.015625)
+# generate a large list of inputs
+dx: float = 0.015
+t: np.ndarray = np.arange(-450, 450+dx, dx)
+theta: np.ndarray = t
 
 ################################################################################
 
-graphs_to_plot = {
+GRAPHS_TO_PLOT: dict[str, tuple[np.ndarray, np.ndarray]] = {
     # # Heart Shape
     # "(16sin³t, 13cost - 5cos2t - 2cos3t - cos4t)": (
     #     16 * np.sin(t)**3,
@@ -27,70 +29,54 @@ graphs_to_plot = {
     ############################################################################
 
     # # Kite Shape
-    # "(rcos³t, rsin³t)": (r * np.cos(t)**3, r * np.sin(t)**3),
+    # "(rcos³t, rsin³t)": (R * np.cos(t)**3, R * np.sin(t)**3),
 
     ############################################################################
 
     # # Lissajous Curve
-    # "(acosk₁t, bsink₂t)": (a * np.cos(k1*t), b * np.sin(k2*t)),
-
-    ############################################################################
-
-    # # Cycloid
-    # "(r(t - sint), r(1 - cost))": (r * (t - np.sin(t)), r * (1 - np.cos(t))),
-
-    ############################################################################
-
-    # # Parabola - 1
-    # "(at², 2at)": (a * t**2, 2*a*t),
-
-    # # Parabola - 2
-    # "(-at², 2at)": (-a * t**2, 2*a*t),
-
-    # # Parabola - 3
-    # "(2at, at²)": (2*a*t, a * t**2),
-
-    # # Parabola - 4
-    # "(2at, -at²)": (2*a*t, -a * t**2),
+    # "(acosk₁t, bsink₂t)": (A * np.cos(K1*t), B * np.sin(K2*t)),
 
     ############################################################################
 
     # # Circle
-    # "(rcosθ, rsinθ)": (r * np.cos(theta), r * np.sin(theta)),
+    # "(rcosθ, rsinθ)": (R*np.cos(theta), R*np.sin(theta)),
 
     ############################################################################
 
-    # # Infinity Symbol - 1
-    # "(rsinθ, rsin2θ)": (r * np.sin(theta), r * np.sin(2*theta)),
-
-    # # Infinity Symbol - 2
-    # "(rsin2θ, rsinθ)": (r * np.sin(2*theta), r * np.sin(theta)),
+    # # Cycloid
+    # "(r(t - sint), r(1 - cost))": (R*(t-np.sin(t)), R*(1-np.cos(t))),
 
     ############################################################################
 
-    # # Ellipse - 1
-    # "(acosθ, bsinθ)": (a * np.cos(theta), b * np.sin(theta)),
-
-    # # Ellipse - 2
-    # "(bcosθ, asinθ)": (b * np.cos(theta), a * np.sin(theta)),
+    # "(at², 2at)": (A * t**2, 2*A*t),                            # Parabola - 1
+    # "(-at², 2at)": (-A * t**2, 2*A*t),                          # Parabola - 2
+    # "(2at, at²)": (2*A*t, A * t**2),                            # Parabola - 3
+    # "(2at, -at²)": (2*A*t, -A * t**2),                          # Parabola - 4
 
     ############################################################################
 
-    # # Hyperbola - 1
-    # "(asecθ, btanθ)": (a * sec(theta), b * np.tan(theta)),
+    # "(rsinθ, rsin2θ)": (R*np.sin(theta), R*np.sin(2*theta)),  # INF Symbol - 1
+    # "(rsin2θ, rsinθ)": (R*np.sin(2*theta), R*np.sin(theta)),  # INF Symbol - 2
 
-    # # Hyperbola - 2
-    # "(btanθ, asecθ)": (b * np.tan(theta), a * sec(theta)),
+    ############################################################################
+
+    # "(acosθ, bsinθ)": (A*np.cos(theta), B*np.sin(theta)),        # Ellipse - 1
+    # "(bcosθ, asinθ)": (B*np.cos(theta), A*np.sin(theta)),        # Ellipse - 2
+
+    ############################################################################
+
+    # "(asecθ, btanθ)": (A/np.cos(theta), B*np.tan(theta)),      # Hyperbola - 1
+    # "(btanθ, asecθ)": (B*np.tan(theta), A/np.cos(theta)),      # Hyperbola - 2
 }
 
 plt.figure(figsize=(10, 5))
 plt.axhline(y=0, color="black", linewidth=2)
 plt.axvline(x=0, color="black", linewidth=2)
 
-for name, (x, y) in graphs_to_plot.items():
+for name, (x, y) in GRAPHS_TO_PLOT.items():
     plt.plot(x, y, marker=".", label=name)
 
-if not graphs_to_plot:
+if not GRAPHS_TO_PLOT:
     print("No graphs to plot")
 else:
     plt.grid(True)
